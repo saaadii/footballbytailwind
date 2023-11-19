9549f58d903eb579237a12dd7f5f1c79b34bdf17
ahmed sadi <ahmedajlem@gmail.com>
2023-11-19 15:36:20 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# manikganjfootball
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+      screens: {
+        '2xl': {'max': '1400px'},
+  
+        'xl': {'max': '1279px'},
+  
+        'lg': {'max': '1023px'},
+  
+        'md': {'max': '767px'},
+  
+        'sm': {'max': '639px'},  
+        },
+      },
+  }
